9108d816736523318c9e0dd326378b719cf17e75
LucioUranga <lurangawhite@udesa.edu.ar>
2025-09-08 16:51:11 -0300

    Hice las rutas

--- proyectointegrador/src/App.js
+++ proyectointegrador/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from "./screens/Home/Home";
+import Detalle from "./screens/Detalle/Detalle";
+import MasVistas from "./screens/MasVistas/MasVistas";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer"
+import Peliculas from "./components/Peliculas/Peliculas"
+
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <Switch>
+        <Route path "/" component={Home} exact={true} />
+        <Route path "/detalle/id" component={Detalle} exact={true} />
+        <Route path "/MasVistas" component={Detalle} exact={true} />
+
+      </Switch>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
