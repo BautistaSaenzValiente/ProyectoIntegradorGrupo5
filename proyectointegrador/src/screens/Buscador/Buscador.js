84c925606a6ae925061e781ff1ac1fc130e5b417
LucioUranga <lurangawhite@udesa.edu.ar>
2025-09-15 17:08:32 -0300

    Busqueda

--- proyectointegrador/src/screens/Buscador/Buscador.js
+++ proyectointegrador/src/screens/Buscador/Buscador.js
@@ -3,10 +3,40 @@ import Card from "../../components/Card/Card";
 import React from "react";
 import Header from "../../components/Header/Header";
 
-function Buscador(){
-    return(
-        <h2>Soy buscador</h2>
-    )
+class Buscador extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            data: [],
+            loading: true
+        }
+    }
+
+    componentDidMount(){
+        console.log(this.props);
+
+        let urlParams = new URLSearchParams(window.location.search)
+        let busqueda = urlParams.get('busqueda')
+        console.log(busqueda);
+    
+        fetch(``)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            this.setState({ data: data.results, loading: false })
+        })
+        
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Header />
+                {this.state.loading ? <p>cargando...</p> : this.state.data.map(unPersonaje => <Card data={unPersonaje} />  )}
+            </React.Fragment>
+        )
+    }
+
 }
 
-export default Buscador
+export default Buscador;
--- proyectointegrador/src/screens/Favoritos/Favoritos.js
+++ proyectointegrador/src/screens/Favoritos/Favoritos.js
@@ -3,6 +3,7 @@ import React from "react";
 function Favoritos(){
     return(
         <h2>Soy Favoritos</h2>
+        
     )
 }
 
