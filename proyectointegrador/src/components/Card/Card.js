4a1d78970c1a855cf72c5e49b97d2bb2a03500ba
BautistaSaenzValiente <bsaenzvaliente@udesa.com.ar>
2025-09-15 17:38:33 -0300

    Favoritos

--- proyectointegrador/src/components/Card/Card.js
+++ proyectointegrador/src/components/Card/Card.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+class Card extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            esFavorito:false
+        }
+    }
 
- const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    agregarAFavoritos(id){
+    let favoritos = []
+    let favoritosLocal = localStorage.getItem(`favoritos`)
+    let favoritosParse = JSON.parse(favoritosLocal)
+
+    if(favoritosParse !== null){
+        favoritosParse.push(id)
+        let favoritosToString = JSON.stringify(favoritosParse)
+        localStorage.setItem(`favoritos`, favoritosToString)
+    }else{
+        favoritos.push(id)
+        let favoritosToString = JSON.stringify(favoritos)
+        localStorage.setItem(`favoritos`, favoritosToString)
+    }
+
+console.log(id)
+
+  }
+  <button onClick={()=>this.state.agregarAFavoritos(id)}>Agregar a favoritos</button>
+
+  componentDidMount(){
+     let favoritosLocal = localStorage.getItem(`favoritos`)
+     let favoritosParse = JSON.parse(favoritosLocal)
+    if(favoritosParse !== null){
+        if(favoritosParse.includes(id)){
+            this.setState({
+                esFavorito: true
+            })
+        }
+    }
+    }
 }
+  
 
-export default Footer;
+export default Card;
--- proyectointegrador/src/screens/Favoritos/Favoritos.js
+++ proyectointegrador/src/screens/Favoritos/Favoritos.js
@@ -7,4 +7,5 @@ function Favoritos(){
     )
 }
 
+
 export default Favoritos
