cf61e868a916baa3e7ba9efed860bce7e3b2bfef
juanchidurini <jdurini@udesa.edu.ar>
2025-09-15 18:05:30 -0300

    sacar de favoritos

--- proyectointegrador/src/components/Card/Card.js
+++ proyectointegrador/src/components/Card/Card.js
@@ -1,44 +1,65 @@
 import React, { Component } from 'react'
-class Card extends Component{
-    constructor(props){
+class Card extends Component {
+    constructor(props) {
         super(props)
         this.state = {
-            esFavorito:false
+            esFavorito: false
         }
     }
 
-    agregarAFavoritos(id){
-    let favoritos = []
-    let favoritosLocal = localStorage.getItem(`favoritos`)
-    let favoritosParse = JSON.parse(favoritosLocal)
+    agregarAFavoritos(id) {
+        let favoritos = []
+        let favoritosLocal = localStorage.getItem(`favoritos`)
+        let favoritosParse = JSON.parse(favoritosLocal)
 
-    if(favoritosParse !== null){
-        favoritosParse.push(id)
-        let favoritosToString = JSON.stringify(favoritosParse)
-        localStorage.setItem(`favoritos`, favoritosToString)
-    }else{
-        favoritos.push(id)
-        let favoritosToString = JSON.stringify(favoritos)
-        localStorage.setItem(`favoritos`, favoritosToString)
-    }
-
-console.log(id)
-
-  }
-  <button onClick={()=>this.state.agregarAFavoritos(id)}>Agregar a favoritos</button>
-
-  componentDidMount(){
-     let favoritosLocal = localStorage.getItem(`favoritos`)
-     let favoritosParse = JSON.parse(favoritosLocal)
-    if(favoritosParse !== null){
-        if(favoritosParse.includes(id)){
-            this.setState({
-                esFavorito: true
-            })
+        if (favoritosParse !== null) {
+            favoritosParse.push(id)
+            let favoritosToString = JSON.stringify(favoritosParse)
+            localStorage.setItem(`favoritos`, favoritosToString)
+            this.setState({ esFavorito: true })
+        } else {
+            favoritos.push(id)
+            let favoritosToString = JSON.stringify(favoritos)
+            localStorage.setItem(`favoritos`, favoritosToString)
+            this.setState({ esFavorito: true })
         }
+
+        console.log(id)
+
     }
+
+    sacarDeFavoritos(id) {
+        let favoritosLocal = localStorage.getItem(`favoritos`)
+        let favoritosParse = JSON.parse(favoritosLocal)
+
+        let quitarFav = favoritosParse.filter(fav => fav !== id)
+
+        let favoritosToString = JSON.stringify(quitarFav)
+        localStorage.setItem(`favoritos`, favoritosToString)
+        this.setState({ esFavorito: false })
+    }
+
+
+    componentDidMount() {
+        let favoritosLocal = localStorage.getItem(`favoritos`)
+        let favoritosParse = JSON.parse(favoritosLocal)
+        if (favoritosParse !== null) {
+            if (favoritosParse.includes(id)) {
+                this.setState({
+                    esFavorito: true
+                })
+            }
+        }
     }
 }
-  
+{
+    this.state.esFavorito ?
+        <button onClick={() => this.sacarAFavoritos(id)}> Sacar de favoritos</button> :
+        <button onClick={() => this.agregarAFavoritos(id)}>Agregar a favoritos</button>
+}
+
+
+
+
 
 export default Card;
