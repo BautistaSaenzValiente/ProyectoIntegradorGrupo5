406e8e8160f22b0b8c72dcb890f5d69b986e0c46
LucioUranga <lurangawhite@udesa.edu.ar>
2025-09-19 14:03:58 -0300

    Avanzo favoritos

--- proyectointegrador/src/screens/Favoritos/Favoritos.js
+++ proyectointegrador/src/screens/Favoritos/Favoritos.js
@@ -1,10 +1,36 @@
+import { Component } from "react";
 import React from "react";
+import Card from "../../components/Card/Card"
 
-function Favoritos(){
-    return(
-        <h2>Soy Favoritos</h2>
-        
-    )
+class Favoritos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favoritos: []
+    };
+  }
+
+  componentDidMount() {
+    let favoritosLocal = localStorage.getItem("favoritos");
+    if (favoritosLocal) {
+      let favoritosParse = JSON.parse(favoritosLocal);
+      this.setState({ favoritos: favoritosParse });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Mis Favoritos</h2>
+        <ul className="cards-container">
+          {this.state.favoritos.length > 0 ? ()
+          : (
+            <p>No tenés favoritos guardados todavía.</p>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Favoritos
+export default Favoritos;
